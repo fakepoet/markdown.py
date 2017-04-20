83fee84c4aa3839c12432b577165a609529c9533
CyberZHG <CyberZHG@gmail.com>
2017-04-20 08:51:52 +0800

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='mosaic',
+    version='0.0.1',
+    author='fatepoet',
+    author_email='',
+    packages=['mosaic'],
+    scripts=[],
+    url='https://github.com/fakepoet/mosaic',
+    license='LICENSE',
+    description='',
+    long_description=open('README.md').read()
+)
