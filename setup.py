1daaafe67963b5761be461b380b20cc100065aaf
Zhao HG <CyberZHG@gmail.com>
2017-04-22 16:35:05 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
 setup(
-    name='mosaic',
+    name='tiezhu',
     version='0.0.1',
-    author='fatepoet',
-    author_email='',
+    author='fakepoet',
+    author_email='fakepoet0101@gmail.com',
     packages=['mosaic'],
     scripts=[],
-    url='https://github.com/fakepoet/mosaic',
+    url='https://github.com/fakepoet/tiezhu',
     license='LICENSE',
     description='',
     long_description=open('README.md').read()
