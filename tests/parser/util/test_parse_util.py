f699456d4e16549c5e375d10cb4c42adc63cb226
CyberZHG <CyberZHG@gmail.com>
2017-05-15 08:45:49 +0800

    #1 Add tests for ParseUtil class.

---
+++ tests/parser/util/test_parse_util.py
@@ -0,0 +1,38 @@
+#!/usr/env/bin python
+import unittest
+from markdown.parser.util import ParseUtil
+
+
+class TestParseUtil(unittest.TestCase):
+
+    def test_get_heading_space_num(self):
+        num = ParseUtil.get_heading_space_num(' \t a', 0)
+        self.assertEqual(6, num)
+        num = ParseUtil.get_heading_space_num(' \t a', 1)
+        self.assertEqual(5, num)
+        num = ParseUtil.get_heading_space_num(' \t a', 2)
+        self.assertEqual(1, num)
+        num = ParseUtil.get_heading_space_num(' \t a', 3)
+        self.assertEqual(0, num)
+
+    def test_is_white_space(self):
+        self.assertTrue(ParseUtil.is_white_space(' '))
+        self.assertTrue(ParseUtil.is_white_space('\t'))
+        self.assertFalse(ParseUtil.is_white_space('a'))
+        self.assertFalse(ParseUtil.is_white_space(u'\u1680'))
+
+    def test_is_unicode_white_space(self):
+        self.assertTrue(ParseUtil.is_unicode_white_space(' '))
+        self.assertTrue(ParseUtil.is_unicode_white_space('\t'))
+        self.assertFalse(ParseUtil.is_unicode_white_space('a'))
+        self.assertTrue(ParseUtil.is_unicode_white_space(u'\u1680'))
+
+    def test_is_ascii_punctuation(self):
+        self.assertFalse(ParseUtil.is_ascii_punctuation(' '))
+        self.assertTrue(ParseUtil.is_ascii_punctuation('@'))
+        self.assertFalse(ParseUtil.is_ascii_punctuation(u'\u2046'))
+
+    def test_is_punctuation(self):
+        self.assertFalse(ParseUtil.is_punctuation(' '))
+        self.assertTrue(ParseUtil.is_punctuation('@'))
+        self.assertTrue(ParseUtil.is_punctuation(u'\u2046'))
